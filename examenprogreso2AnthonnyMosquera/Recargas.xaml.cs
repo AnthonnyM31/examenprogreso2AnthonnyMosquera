c7dc734a4d1f5d868538fc1cb23e9baf7a9f1b9a
AnthonnyM31 <anthonnymosquera1492@gmail.com>
2024-12-02 08:05:53 -0500

    creamos la clase recarga y el metodo para guardarlas

--- examenprogreso2AnthonnyMosquera/Recargas.xaml.cs
+++ examenprogreso2AnthonnyMosquera/Recargas.xaml.cs
@@ -1,3 +1,5 @@
+//using static Android.Provider.ContactsContract.CommonDataKinds;
+
 namespace examenprogreso2AnthonnyMosquera;
 
 public partial class Recargas : ContentPage
@@ -6,4 +8,45 @@ public partial class Recargas : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    public class Recarga
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        //si no están marcadas como opcionales o requeridas no se por que pero no funciona asi que vamos a permitir valores null
+        public string? Titulo { get; set; }
+        public string? Contenido { get; set; }
+        public DateTime FechaHora { get; set; } = DateTime.Now;
+    }
+
+
+
+
+    private async void GuardarRecargaClicked(object sender, EventArgs e)
+    {
+        string numero = amosqueranumero.Text;
+        string contenido = amosquerarecarga.Text;
+
+        if (!string.IsNullOrWhiteSpace(numero) && !string.IsNullOrWhiteSpace(contenido))
+        {
+            var nuevaRecarga = new Recarga
+            {
+                Titulo = numero,
+                Contenido = contenido,
+                FechaHora = DateTime.Now
+            };
+
+            await App.Database.GuardarNotaAsync(nuevaRecarga);
+
+            await DisplayAlert("Nota Guardada", "Tu nota ha sido guardada con éxito.", "OK");
+            await Navigation.PopAsync();
+        }
+        else
+        {
+            await DisplayAlert("Error", "Por favor, completa todos los campos.", "OK");
+        }
+    }
+
 }
